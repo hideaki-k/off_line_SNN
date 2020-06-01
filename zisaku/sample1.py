bd40a424aa816d215f35148a3f5c59e15aa63910
hideaki-k <h.kinoshita@kimura-lab.net>
2020-06-01 23:30:10 +0900

    printdebug

--- zisaku/sample1.py
+++ zisaku/sample1.py
@@ -4,11 +4,13 @@ from scipy.special import erf
 
 import pylab
 import nest
+print("1")
 neuron = nest.Create("iaf_psc_alpha")
 nest.GetStatus(neuron)
 nest.GetStatus(neuron, "I_e")
 nest.GetStatus(neuron, ["V_reset", "V_th"])
 nest.SetStatus(neuron, {"I_e": 376})
+print("2")
 multimeter = nest.Create("multimeter")
 nest.SetStatus(multimeter, {"withtime":True, "record_from":["V_m"]})
 spikedetector = nest.Create("spike_detector",
@@ -16,5 +18,5 @@ spikedetector = nest.Create("spike_detector",
 
 nest.Connect(multimeter, neuron)
 nest.Connect(neuron, spikedetector)
-
+print("3")
 nest.Simulate(1000.0)
