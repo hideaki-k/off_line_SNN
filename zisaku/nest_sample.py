074203424105de9d19e7febed80492a8dddcf1ca
hideaki-k <h.kinoshita@kimura-lab.net>
2020-06-03 21:19:50 +0900

    Update nest_sample.py

--- zisaku/nest_sample.py
+++ zisaku/nest_sample.py
@@ -1,40 +1,45 @@
+"""
+Simple network with a Poisson spike source projecting to a pair of IF_curr_alpha neurons
+
+Andrew Davison, UNIC, CNRS
+August 2006
+
+
+"""
+
 import numpy
-from pyNN.nest import *
-import matplotlib.pyplot as plt
+from pyNN.utility import get_script_args
 
+simulator_name = get_script_args(1)[0]  
+exec("from pyNN.%s import *" % simulator_name)
 
-def test_sim(on_or_off_grid, sim_time):
-    setup(timestep=1.0, min_delay=1.0, max_delay=1.0, spike_precision=on_or_off_grid)
-    src = Population(1, SpikeSourceArray(spike_times=[0.5]))
-    cm = 250.0
-    tau_m = 10.0
-    tau_syn_E = 1.0
-    weight = cm / tau_m * numpy.power(tau_syn_E / tau_m, -tau_m / (tau_m - tau_syn_E)) * 20.5
-    nrn = Population(1, IF_curr_exp(cm=cm, tau_m=tau_m, tau_syn_E=tau_syn_E,
-                                    tau_refrac=2.0, v_thresh=20.0, v_rest=0.0,
-                                    v_reset=0.0, i_offset=0.0))
-    nrn.initialize(v=0.0)
-    prj = Projection(src, nrn, OneToOneConnector(), StaticSynapse(weight=weight))
-    nrn.record('v')
-    run(sim_time)
-    return nrn.get_data().segments[0].analogsignals[0]
+tstop = 1000.0
+rate = 100.0
 
-sim_time = 10.0
-off = test_sim('off_grid', sim_time)
-on = test_sim('on_grid', sim_time)
+setup(timestep=0.1, min_delay=0.2, max_delay=1.0)
+    
+cell_params = {'tau_refrac':2.0,'v_thresh':-50.0,'tau_syn_E':2.0, 'tau_syn_I':2.0}
+output_population = Population(2, IF_curr_alpha, cell_params, label="output")
 
+number = int(2*tstop*rate/1000.0)
+numpy.random.seed(26278342)
+spike_times = numpy.add.accumulate(numpy.random.exponential(1000.0/rate, size=number))
+assert spike_times.max() > tstop
+print spike_times.min()
 
-def plot_data(pos, on, off, ylim, with_legend=False):
-    ax = plt.subplot(1, 2, pos)
-    ax.plot(off.times, off, color='0.7', linewidth=7, label='off-grid')
-    ax.plot(on.times, on, 'k', label='on-grid')
-    ax.set_ylim(*ylim)
-    ax.set_xlim(0, 9)
-    ax.set_xlabel('time [ms]')
-    ax.set_ylabel('V [mV]')
-    if with_legend:
-        plt.legend()
+input_population  = Population(1, SpikeSourceArray, {'spike_times': spike_times}, label="input")
 
-plot_data(1, on, off, (-0.5, 21), with_legend=True)
-plot_data(2, on, off, (-0.05, 2.1))
-plt.show()
+projection = Projection(input_population, output_population, AllToAllConnector())
+projection.setWeights(1.0)
+
+input_population.record()
+output_population.record()
+output_population.record_v()
+
+run(tstop)
+
+output_population.printSpikes("Results/simpleNetwork_output_%s.ras" % simulator_name)
+input_population.printSpikes("Results/simpleNetwork_input_%s.ras" % simulator_name)
+output_population.print_v("Results/simpleNetwork_%s.v" % simulator_name)
+
+end()
