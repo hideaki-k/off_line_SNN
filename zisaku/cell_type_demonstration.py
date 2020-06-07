289a58e54bf850372383a10f5e4c4e9c464053e2
hideaki-k <h.kinoshita@kimura-lab.net>
2020-06-07 12:42:39 +0900

    Update cell_type_demonstration.py

--- zisaku/cell_type_demonstration.py
+++ zisaku/cell_type_demonstration.py
@@ -12,48 +12,54 @@ optional arguments:
   --debug        Print debugging information
 
 """
+
 from pyNN.utility import get_simulator, init_logging, normalized_filename
 
-# === Configure the simulator ===================================================
-sim, options = get_simulator(("--plot-figure","Plot the simulation results to a file.",{"action":"stone_true"}),
-                ("--debug", "Print debugging information"))
+
+# === Configure the simulator ================================================
+
+sim, options = get_simulator(("--plot-figure", "Plot the simulation results to a file.", {"action": "store_true"}),
+                             ("--debug", "Print debugging information"))
 
 if options.debug:
     init_logging(None, debug=True)
 
 sim.setup(timestep=0.01, min_delay=1.0)
 
-# === Build and instrument the network ==========================================
 
-cuba_exp = sim.Population(1, sim.IF_curr_exp(i_offset-1.0),label="IF_curr_exp")
-hh = sim.Populaton(1, sim.HH_cond_exp(i_offset=0.2), label="HH_cond_exp")
-adexp = sim.Population(1, sim.IF_cond_exp(isfa_ista(i_offset=1.0),label="EIF_cond_exp_isfa_ista"))
-adapt = sim.Population(1, sim.IF_cond_exp_gsfa_grr(i_offset=2.0),label="IF_cond_exp_gsfa_grr")
-izh = sim.Population(1, sim.Izikevich(i_offset=0.01),label="Izhikevich")
+# === Build and instrument the network =======================================
 
-all_neurons = cuba_exp + hh + adapt + izh
+cuba_exp = sim.Population(1, sim.IF_curr_exp(i_offset=1.0), label="IF_curr_exp")
+hh = sim.Population(1, sim.HH_cond_exp(i_offset=0.2), label="HH_cond_exp")
+adexp = sim.Population(1, sim.EIF_cond_exp_isfa_ista(i_offset=1.0), label="EIF_cond_exp_isfa_ista")
+adapt = sim.Population(1, sim.IF_cond_exp_gsfa_grr(i_offset=2.0), label="IF_cond_exp_gsfa_grr")
+izh = sim.Population(1, sim.Izhikevich(i_offset=0.01), label="Izhikevich")
 
-all_neurons.record("v")
-adexp.record("w")
-izh.record("u")
+all_neurons = cuba_exp + hh + adexp + adapt + izh
 
-# === Run the simulation ==========================================================
+all_neurons.record('v')
+adexp.record('w')
+izh.record('u')
+
+
+# === Run the simulation =====================================================
 
 sim.run(100.0)
 
-# ===Save the results, optionally plot a figure ==================================
 
-filename = normalized_filename("Results","cell_type_demonstration","pkl",options.simulator)
-all_neurons.write_data(filename, annotations={"script_name":__file__})
+# === Save the results, optionally plot a figure =============================
+
+filename = normalized_filename("Results", "cell_type_demonstration", "pkl", options.simulator)
+all_neurons.write_data(filename, annotations={'script_name': __file__})
 
 if options.plot_figure:
     from pyNN.utility.plotting import Figure, Panel
-    figure_filename = filename.replace("pkl","png")
+    figure_filename = filename.replace("pkl", "png")
     Figure(
-        Panel(cuba_exp.get_data().segments[0].filter(name="v")[0],
-                ylabel="Membrane Potential(mV)",
-                data_labels=[cuba_exp.label],yticks=True,ylim=(-66,-48)),
-                Panel(hh.get_data().segments[0].filter(name='v')[0],
+        Panel(cuba_exp.get_data().segments[0].filter(name='v')[0],
+              ylabel="Membrane potential (mV)",
+              data_labels=[cuba_exp.label], yticks=True, ylim=(-66, -48)),
+        Panel(hh.get_data().segments[0].filter(name='v')[0],
               ylabel="Membrane potential (mV)",
               data_labels=[hh.label], yticks=True, ylim=(-100, 60)),
         Panel(adexp.get_data().segments[0].filter(name='v')[0],
@@ -72,11 +78,11 @@ if options.plot_figure:
               xticks=True, xlabel="Time (ms)",
               ylabel="u (mV/ms)",
               data_labels=[izh.label], yticks=True, ylim=(-14, 0)),
-              title="responses of standard neuro model to current injection",
-              annotations="simulated with %s" % options.simulator.upper()
+        title="Responses of standard neuron models to current injection",
+        annotations="Simulated with %s" % options.simulator.upper()
     ).save(figure_filename)
     print(figure_filename)
 
 # === Clean up and quit ========================================================
-# 
-# sim.end(9)  
+
+sim.end()
