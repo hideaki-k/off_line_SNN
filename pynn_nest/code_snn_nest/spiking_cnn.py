ddbdfd044d04be0152a514188b55c3b232f6113c
hideaki-k <h.kinoshita@kimura-lab.net>
2020-06-07 17:36:51 +0900

    k_size  cast

--- pynn_nest/code_snn_nest/spiking_cnn.py
+++ pynn_nest/code_snn_nest/spiking_cnn.py
@@ -115,6 +115,7 @@ def construct_layer(cell_params_lif, pop_list_in, mode, k_size, w_layer):
     syn = 5.*0.96
     in_num = len(pop_list_in) #populations number in previous layer
     in_size = int(np.sqrt(pop_list_in[0].size)) #in_size*in_size = neuron_num per pop in the previous layer
+    k_size = int(k_size)
     print("k_size",type(k_size))
     print("mode : ",mode)
     pop_layer = []
